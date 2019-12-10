0f72db97b554d5f4860bdc7d6a1c6328bd45eaac
vampir490 <victornik90@gmail.com>
2019-12-10 20:59:20 +0300

    bootstrap installed

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -51,7 +51,7 @@ class EventsController < ApplicationController
       @event = Event.find(params[:id])
     end
 
-    def event_params
-      params.require(:event).permit(:title, :address, :datetime, :description)
-    end
+  def event_params
+    params.require(:event).permit(:title, :address, :datetime, :description)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+  has_many :events
+
+  validates :name, presence: true, length: {maximum: 35}
+  validates :email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+
 end
