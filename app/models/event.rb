ce4f78c923c2b31fbddfabf4a99f2d6360f20c7c
vampir490 <victornik90@gmail.com>
2019-12-25 14:20:40 +0300

    modified event datetime validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,13 +8,5 @@ class Event < ApplicationRecord
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
   validates :datetime, presence: true
-
-  validates :datetime, inclusion: { in: Time.now..,
-                                    message: I18n.t('activerecord.validations.error_event_in_past') }
-
-  #def datetime_cannot_be_in_the_past
-  #  if datetime < Time.now
-  #    errors.add(:datetime, I18n.t(activerecord.validations.error_event_in_past))
-  #  end
-  #end
+  validates :datetime, inclusion: { in: Time.now.. }, on: :create
 end
