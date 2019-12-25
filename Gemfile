beac891e5b9ca7195daa781de016c8cfc0189880
vampir490 <victornik90@gmail.com>
2019-12-25 21:39:28 +0300

    Added avatars to users and photos to events

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'devise'
 gem 'devise-i18n'
 gem 'rails-i18n', '~> 5.1'
 
+gem 'carrierwave'
+gem 'rmagick'
+
 gem 'twitter-bootstrap-rails'
 
 gem 'uglifier', '>= 1.3.0'
--- app/models/event.rb
+++ app/models/event.rb
@@ -4,9 +4,14 @@ class Event < ApplicationRecord
   has_many :comments
   has_many :subscriptions
   has_many :subscribers, through: :subscriptions, source: :user
+  has_many :photos
 
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
   validates :datetime, presence: true
   validates :datetime, inclusion: { in: Time.now.. }, on: :create
+
+  def visitors
+    (subscribers + [user]).uniq
+  end
 end
