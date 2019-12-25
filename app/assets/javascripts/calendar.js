1945cf323804cffe914c26ae23afa38d356b7a26
vampir490 <victornik90@gmail.com>
2019-12-25 14:39:39 +0300

    modified calendar js

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   flatpickr('#flatpickr-input',
     {
       enableTime: true,
-      minDate: "today",
       defaultHour: 20,
       defaultMinute: 30,
       dateFormat: "d-m-Y H:i",
