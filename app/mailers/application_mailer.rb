08c3d66f93132afea2640d681d346e941192de3d
vampir490 <victornik90@gmail.com>
2020-01-29 22:00:19 +0300

    sendgrid modified for prod

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'bbq@cynepbbq.com'
+  default from: 'bbq@burundukmedia.ru'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'bbqcynep.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'burundukmedia.ru'}
  
   # Ошибки рассылки юзеру не показываем
   config.action_mailer.raise_delivery_errors = false
@@ -106,11 +106,11 @@ Rails.application.configure do
   # А это для SendGrid
     ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :port           => '465',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :user_name      => ENV['sendgrid_username'],
+    :password       => ENV['sendgrid_password'],
+    :domain         => 'burundukmedia.ru',
     :enable_starttls_auto => true
   }
 end
